9be8920f8a71cf5e29c8dc442815afc5515393f2
Sunder314 <161341743+Sunder314@users.noreply.github.com>
2024-10-11 17:24:17 +0200

    Update Numpy_IA.py

--- pythonProject/Fonction_base_numpy_array/Numpy_IA.py
+++ pythonProject/Fonction_base_numpy_array/Numpy_IA.py
@@ -1,3 +1,3 @@
 import numpy as np
-myArray = np.arange(10).reshape(2,5)
+myArray = np.arange(10).reshape(2,5) # Crée une matrice de 10 nombre (0 à 9) sous la forme (5(colones),2(lignes))
 print(myArray)
